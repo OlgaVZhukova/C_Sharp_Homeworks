7b5b57a849c92edc799c08f8eab782091784b932
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-02 01:57:16 +0300

    Lesson1 Task2 updated

--- Lesson1_Task2/Program.cs
+++ Lesson1_Task2/Program.cs
@@ -5,4 +5,32 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+System.Console.WriteLine("Введите первое число, пожалуйста: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число, пожалуйста: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число, пожалуйста: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
 
+if(number1 > number2)
+{
+    if(number1 > number3)
+    {
+        System.Console.WriteLine($"Число {number1} является максимальным из этих чисел.");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number3} является максимальным из этих чисел.");
+    }
+}
+else
+{
+    if(number2 > number3)
+    {
+        System.Console.WriteLine($"Число {number2} является максимальным из этих чисел.");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number3} является максимальным из этих чисел.");
+    }
+}
