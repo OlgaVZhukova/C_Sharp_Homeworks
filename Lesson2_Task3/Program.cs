40ebcf9fa8b838c55ae554e69f5b8cad59224879
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-06 01:06:28 +0300

    Lesson2 Task3 added

---
+++ Lesson2_Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+System.Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+if (number.Length > 2)
+{
+  System.Console.WriteLine("Третья цифра - " + number[2]);
+}
+else {
+  System.Console.WriteLine("Третьей цифры нет");
+}
