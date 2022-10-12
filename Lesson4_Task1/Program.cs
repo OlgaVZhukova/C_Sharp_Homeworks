e60addbba4867ed00d3d38bc11d6254ac2fcf651
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-12 21:39:21 +0300

    Seminar 4 Task 4 ad added

--- Lesson4_Task1/Program.cs
+++ Lesson4_Task1/Program.cs
@@ -2,4 +2,44 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int InputNumber(string msg)
+{
+    System.Console.WriteLine(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int A = InputNumber("Введите первое число: ");
+int B = InputNumber("Введите второе число: ");
+
+// int degree = 0;
+// for (int B = 0; B <= A; i++)
+// {
+//     degree = degree * i;
+// }
+
+int Degree(int number)
+{
+    int x = A;
+    int y = B;
+    
+}
+
+
+
+
+int Degree(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(A);
+System.Console.WriteLine(B);
+
+
 
