7e125c2ec4e49f756c7b5283a52b0fa88566735e
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-13 21:48:11 +0300

    Lesson 4 Task 3 updated

--- Lesson4_Task3/Program.cs
+++ Lesson4_Task3/Program.cs
@@ -2,5 +2,14 @@
 // 1, 2, 5, 7, 19, 6, 7, 8 -> [1, 2, 5, 7, 19, 6, 7, 8]
 // 0, 6, 1, 33, 4, 7, 6, 8 -> [0, 6, 1, 33, 4, 7, 6, 8]
 
+// 1, 2, 5, 7, 19, 6, 7, 8
 
 
+int [] array = new int[8];
+Random myRandom = new Random();
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = myRandom.Next(0, 100);
+    Console.Write(array[i] + " ");
+}
