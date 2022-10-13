e42e02f6d48d55072a9c84030bb3d53bebfcb2f7
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-13 20:22:35 +0300

    Lesson 4 Task 2 updated

--- Lesson4_Task2/Program.cs
+++ Lesson4_Task2/Program.cs
@@ -3,4 +3,37 @@
 // 82 -> 10
 // 9012 -> 12
 
+int InputNumber(string message)
+{
+	System.Console.WriteLine(message);
+	int number = Convert.ToInt32(Console.ReadLine());
+	return number;
+}
 
+int Count(int number)
+{
+	int result = 0;
+	while (number > 0)
+	{
+		number = number / 10;
+		result++;
+	}
+	return result;
+}
+
+int number = InputNumber("Введите число: ");
+int result = Count(number);
+
+int SumNumbers(int number)
+{
+	int sum = 0;
+	for (int i = 1; i <= result; i++)
+	{
+		sum += number % 10;
+		number = number / 10;
+	}
+	return sum;
+}
+
+int sum = SumNumbers(number);
+System.Console.WriteLine($"Сумма цифр числа равна: {sum}");
