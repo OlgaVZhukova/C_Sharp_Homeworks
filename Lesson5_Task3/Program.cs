3b71ca6bba239915319c78fad93bce723aa18dfd
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-15 19:21:47 +0300

    Lesson5 Task3 updated

--- Lesson5_Task3/Program.cs
+++ Lesson5_Task3/Program.cs
@@ -1,5 +1,63 @@
-﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
 // [3 7 22 2 78] -> 76
 
+int[] CreateArray(int length, int minLimit, int maxLimit)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
 
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Max(int[] array)
+{
+    int max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int Min(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int[] arr = CreateArray(5, 1, 100);
+PrintArray(arr);
+int min = Min(arr);
+int max = Max(arr);
+
+int DiffMaxMin(int[] array)
+{
+    int difference = max - min;
+    return difference;
+}
+
+int result = DiffMaxMin(arr);
+System.Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {result}.");
 
