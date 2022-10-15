a4ac70d09511554b30c03b077e1117dd5a15f376
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-15 11:47:32 +0300

    Lesson 4 Task 1 updated

--- Lesson4_Task1/Program.cs
+++ Lesson4_Task1/Program.cs
@@ -2,33 +2,54 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int InputNumber(string msg)
+// Мое решение:
+// int InputNumber(string msg)
+// {
+//     System.Console.WriteLine(msg);
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     return number;
+// }
+
+// int A = InputNumber("Введите первое число: ");
+// int B = InputNumber("Введите второе число: ");
+
+// int DegreeResult(int number)
+// {
+// 	int degree = A;
+// 	for (int i = 1; i <= B - 1; i++)
+// 	{
+// 		degree = degree * A;
+// 	}
+// 	return degree;
+// }
+
+// int result = DegreeResult(A);
+// if (B == 0)
+//     {
+//         System.Console.WriteLine("Результат возведения числа в нулевую степень равен: 1");
+//     }
+// else
+// {
+//     System.Console.WriteLine($"Результат возведения первого числа в степень второго числа равен: {result}");;
+// }
+
+// Решение с семинара:
+
+System.Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (B >= 0)
 {
-    System.Console.WriteLine(msg);
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
-}
-
-int A = InputNumber("Введите первое число: ");
-int B = InputNumber("Введите второе число: ");
-
-int DegreeResult(int number)
-{
-	int degree = A;
-	for (int i = 1; i <= B - 1; i++)
-	{
-		degree = degree * A;
-	}
-	return degree;
-}
-
-int result = DegreeResult(A);
-if (B == 0)
+    int result = 1;
+    for (int i = 0; i < B; i++)
     {
-        System.Console.WriteLine("Результат возведения числа в нулевую степень равен: 1");
+        result = result * A;
     }
+    System.Console.WriteLine($"Число {A} в степени {B} равно {result}");
+}
 else
 {
-    System.Console.WriteLine($"Результат возведения первого числа в степень второго числа равен: {result}");;
+    System.Console.WriteLine($"Число {B} не натуральное");
 }
-
