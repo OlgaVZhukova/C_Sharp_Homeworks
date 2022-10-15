5aa3c49e1c59adbcdc1dd1d855634699ff214ad8
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-15 12:04:08 +0300

    Lesson 4 Task 3 updated

--- Lesson4_Task3/Program.cs
+++ Lesson4_Task3/Program.cs
@@ -1,13 +1,36 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-
-int [] array = new int[8];
-Random myRandom = new Random();
+// мое решение:
+// int [] array = new int[8];
+// Random myRandom = new Random();
  
-for (int i = 0; i < array.Length; i++)
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = myRandom.Next(0, 100);
+//     Console.Write(array[i] + " ");
+// }
+
+// Преподаватель: Тут надо сделать через 2 функции, одна задает массив, вторая его выводит.
+
+// Решение на семинаре:
+
+int[] CreateArray()
 {
-    array[i] = myRandom.Next(0, 100);
-    Console.Write(array[i] + " ");
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 21);
+    }
+    return array;
 }
 
-// Тут надо сделать через 2 функции, одна задает массив, вторая его выводит.
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] arr = CreateArray();
+PrintArray(arr);
