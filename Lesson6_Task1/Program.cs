fb28a016b017b26212ddabd48fb23278e272d89b
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-21 19:00:03 +0300

    Lesson 6 Task 1 updated

--- Lesson6_Task1/Program.cs
+++ Lesson6_Task1/Program.cs
@@ -3,42 +3,31 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+//Я очень долго мучилась и хоть убейте не пойму, почему мой код не работает... Надеюсь на ваш комментарий. Спасибо
+
 System.Console.Write("Сколько чисел вы бы хотели ввести: ");
 int quantity = Convert.ToInt32(Console.ReadLine());
 
-// int InputNumber(string message)
-// {
-//     Console.Write(message);
-//     int number = Convert.ToInt32(Console.ReadLine());
-//     for (int i = 1; i < length; i++)
-//     {
-        
-//     }
-//     return number;
-// }
-
-// int n = InputNumber("Введите число:");
-
-int[] Numbers(int[] array)
+int[] InputNum()
 {
     int length = quantity;
-    int[] arr = new int[length];
-    for (int i = 1; i <= arr.Length; i++)
+    int[] numbers = new int[length];
+    for (int i = 1; i <= numbers.Length; i++)
     {
         System.Console.Write($"Введите число под номером {i}: ");
-        arr[i] = Convert.ToInt32(Console.ReadLine());
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
     }
-    return arr;
+    return numbers;
 }
 
-int[] numbers = Numbers(arr);
+int[] num = InputNum();
 
 int QuantityPositive()
 {
     int count = 0;
-    for (int i = 0; i < quantity; i++)
+    for (int i = 0; i <= quantity; i++)
     {
-        if (numbers[i] > 0)
+        if (num[i] > 0)
         {
             count++;
         }
@@ -46,80 +35,38 @@ int QuantityPositive()
     return count;
 }
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < quantity; i++)
-    {
-        System.Console.Write($"{array[i]}\t");
-    }
-    System.Console.WriteLine();
-}
+int result = QuantityPositive();
+System.Console.WriteLine($"Количество введенных чисел больше нуля, равно: + {result}");
 
-PrintArray(QuantityPositive(array));
 
-int count = QuantityPositive();
-// System.Console.WriteLine(numbers);
-System.Console.WriteLine("Количество введенных чисел больше нуля, равно: " + count);
 
-// void Prompt(string[] msg)
+// И был у меня и такой вариант:
+// System.Console.Write("Сколько чисел вы бы хотели ввести: ");
+// int quantity = Convert.ToInt32(Console.ReadLine());
+
+// int[] InputNumber()
 // {
-// //     System.Console.Write("Введите количество чисел, которые хотите ввести:\t");
-//     // int quantity = Convert.ToInt32(Console.ReadLine());
-//     int[] myArray = new int[quantity];
-//     for (int i = 1; i <= myArray.Length; i++)
+//     int length = quantity;
+//     int[] numbers = new int[length];
+//     for (int i = 1; i <= length; i++)
 //     {
-//         System.Console.WriteLine($"Введите число под номером {i}:");
-//         myArray[i] = Convert.ToInt32(Console.ReadLine());
+//         System.Console.Write($"Введите число под номером {i}: ");
+//         numbers[i] = Convert.ToInt32(Console.ReadLine());
 //     }
-//     System.Console.WriteLine("Вывод массива:");
-// //     for (int j = 0; j < myArray.Length; j++)
-//     {
-//         System.Console.WriteLine(myArray[j]);
-//     }
-//     return myArray;    //Console.WriteLine(); // Console.ReadLine();
+//     return numbers;
 // }
 
-// int array = Prompt(myArray);
+// int[] num = InputNumber();
 
-// int[] Array(string[] args)
+// int QuantityPositive(int number)
 // {
-//     int[] arr = new int[10];
-//     for (int i = 1; i < arr.Length; i++)
+//     int count = 0;
+//     while (num[i] > 0)
 //     {
-//         arr[i] = Convert.ToInt32(Console.ReadLine());
+//         count++;
 //     }
-//     System.Console.WriteLine(arr[4]);
-//     Console.ReadLine();
-//     return arr;
+//     return count;
 // }
 
-// int[] arr = Array(arr);
-
-
-// void PrintArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         System.Console.Write($"{array[i]}\t");
-//     }
-//     System.Console.WriteLine();
-// }
-
-// int Count(int[] array)
-// {
-// 	int count = 0;
-// 	for (int i = 0; i <= array.Length; i++)
-// 	{
-// 		if(array[i] > 0)
-// 		{
-// 			count++;
-// 		{
-// 	}
-// 	return count;
-// }
-
-// PrintArray(array);
-// Prompt("Введите число:");
-// int result = Count(array);
-// System.Console.WriteLine(result); //($"Количество введенных чисел больше нуля, равно: {result}")
-
+// int result = QuantityPositive(number);
+// System.Console.WriteLine($"Количество введенных чисел больше нуля, равно: + {result}");
