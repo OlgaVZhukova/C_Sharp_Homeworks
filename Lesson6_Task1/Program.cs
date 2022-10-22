3c05a433a07321dc7e197194e8cbfb1034963c1e
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-22 10:17:04 +0300

    Lesson 6 Task 1 updated

--- Lesson6_Task1/Program.cs
+++ Lesson6_Task1/Program.cs
@@ -3,29 +3,23 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-//Я очень долго мучилась и хоть убейте не пойму, почему мой код не работает... Надеюсь на ваш комментарий. Спасибо
 
-System.Console.Write("Сколько чисел вы бы хотели ввести: ");
-int quantity = Convert.ToInt32(Console.ReadLine());
-
-int[] InputNum()
+int[] InputNum(int quantity)
 {
     int length = quantity;
     int[] numbers = new int[length];
-    for (int i = 1; i <= numbers.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        System.Console.Write($"Введите число под номером {i}: ");
+        System.Console.Write($"Введите число под номером {i + 1}: ");
         numbers[i] = Convert.ToInt32(Console.ReadLine());
     }
     return numbers;
 }
 
-int[] num = InputNum();
-
-int QuantityPositive()
+int QuantityPositive(int[] num)
 {
     int count = 0;
-    for (int i = 0; i <= quantity; i++)
+    for (int i = 0; i < num.Length; i++)
     {
         if (num[i] > 0)
         {
@@ -35,38 +29,11 @@ int QuantityPositive()
     return count;
 }
 
-int result = QuantityPositive();
+System.Console.Write("Сколько чисел вы бы хотели ввести: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+int[] num = InputNum(quantity);
+
+
+int result = QuantityPositive(num);
 System.Console.WriteLine($"Количество введенных чисел больше нуля, равно: + {result}");
-
-
-
-// И был у меня и такой вариант:
-// System.Console.Write("Сколько чисел вы бы хотели ввести: ");
-// int quantity = Convert.ToInt32(Console.ReadLine());
-
-// int[] InputNumber()
-// {
-//     int length = quantity;
-//     int[] numbers = new int[length];
-//     for (int i = 1; i <= length; i++)
-//     {
-//         System.Console.Write($"Введите число под номером {i}: ");
-//         numbers[i] = Convert.ToInt32(Console.ReadLine());
-//     }
-//     return numbers;
-// }
-
-// int[] num = InputNumber();
-
-// int QuantityPositive(int number)
-// {
-//     int count = 0;
-//     while (num[i] > 0)
-//     {
-//         count++;
-//     }
-//     return count;
-// }
-
-// int result = QuantityPositive(number);
-// System.Console.WriteLine($"Количество введенных чисел больше нуля, равно: + {result}");
