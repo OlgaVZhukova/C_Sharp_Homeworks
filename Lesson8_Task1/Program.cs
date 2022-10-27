0b04562aa22a55a4c9e7596b52797e7b4574ee55
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-27 19:01:45 +0300

    Lesson 8 Task 1 updated

--- Lesson8_Task1/Program.cs
+++ Lesson8_Task1/Program.cs
@@ -8,5 +8,54 @@
 2 3 5 9
 2 4 4 8 */
 
+int[,] CreateArray(int lenRows, int lenColumns)
+{
+    int[,] array = new int[lenRows, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+    return array;
+}
 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
 
+int[,] array = CreateArray(3, 4);
+PrintArray(array);
+
+void SortToLowerArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+SortToLowerArray(array);
+System.Console.WriteLine();
+PrintArray(array);
