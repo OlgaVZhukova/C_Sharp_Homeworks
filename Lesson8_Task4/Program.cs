83df1c17b24828eed6411f30ad5123382f1ef393
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-28 19:28:24 +0300

    Lesson 8 Task 4 added

---
+++ Lesson8_Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+
+int[,,] CreateArray(int lenRows, int lenColumns, int lenDepth)
+{
+    int[,,] array = new int[lenRows, lenColumns, lenDepth];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = random.Next(10, 100, 100);
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                System.Console.Write($"{array[i, j, k]}\t");
+            }
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,,] array = CreateArray(2, 2, 2);
+PrintArray(array);
+
+
