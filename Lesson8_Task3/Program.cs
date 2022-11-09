967b3e30a76d867474773580798a86e8a11b3bbd
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-09 18:24:13 +0300

    Lesson 8 Task 3 updated

--- Lesson8_Task3/Program.cs
+++ Lesson8_Task3/Program.cs
@@ -1,19 +1,4 @@
-﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-// Например, заданы 2 массива:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 5 2 6 7
-// и
-// 1 5 8 5
-// 4 9 4 2
-// 7 2 2 6
-// 2 3 4 7
-// Их произведение будет равно следующему массиву:
-// 1 20 56 10
-// 20 81 8 6
-// 56 8 4 24
-// 10 6 24 49
+﻿// Задача 3: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
 int Prompt(string message)
 {
@@ -48,7 +33,7 @@ void PrintArray(int[,] array)
 }
 
 int quantityRow = Prompt("Введите количество строк матрицы: ");
-int quantityColumn = Prompt("Введите количество столбцов матрицы: ");
+int quantityColumn = Prompt("Введите такое же количество столбцов матрицы: ");
 int[,] array = CreateArray(quantityRow, quantityColumn);
 int[,] secondArray = CreateArray(quantityRow, quantityColumn);
 PrintArray(array);
@@ -57,7 +42,7 @@ PrintArray(secondArray);
 
 int[,] ProductArrays(int quantityRow, int quantityColumn)
 {
-    int[,] resultArray = new int[quantityRow, quantityColumn]; //new array[int quantityRow, int quantityColumn]
+    int[,] resultArray = new int[quantityRow, quantityColumn];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < secondArray.GetLength(0); j++)
