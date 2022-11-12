bc0ece9aaa5a6ceddb60161109c942c5ea71ea21
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 12:15:01 +0300

    Seminar 9 Task 1 updated

--- Lesson9_Task1/Program.cs
+++ Lesson9_Task1/Program.cs
@@ -2,4 +2,21 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
 
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
