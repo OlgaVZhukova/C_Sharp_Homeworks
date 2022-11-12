1fef846e00c348680af653cd173e01ed9e476802
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-11-12 12:25:05 +0300

    Seminar 9 Task 2 updated

--- Lesson9_Task2/Program.cs
+++ Lesson9_Task2/Program.cs
@@ -2,4 +2,31 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
 
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+int m = Prompt("Введите число M: ");
+int n = Prompt("Введите число N: ");
+
+int SumMN(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + SumMN(m, n);
+        return result;
+    }
+}
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write($"Cумма чисел от M до N равняется {SumMN(m - 1, n)} ");
+}
+
+SumFromMToN(m, n);
